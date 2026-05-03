d4da52c943b01db6e382ea12dea7615154c036b7
Mirko Passeri <bionikle746@gmal.com>
2026-05-03 16:03:01 +0200

    Aggiunto il componente ToggleSwitch per migliorare l'accessibilità e l'interattività. Aggiornato il file tokens.css per includere variabili relative al nuovo componente. Modificato cardcomponents.css per migliorare la stilizzazione. Creata la pagina toggle.astro per integrare il nuovo componente.

---
+++ src/components/a11y/ToggleSwitch.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <span id="switch-label">{{ props.label }}</span>
+    <button @click="isChecked = !isChecked" role="switch" :aria-checked="isChecked.toString()"
+      aria-labelledby="switch-label">
+      <span class="track" :class="{ 'is-active': isChecked }">
+        <span class="thumb"></span>
+      </span>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  label: {
+    type: String,
+    default: 'Event Switch'
+  },
+  checked: {
+    type: Boolean,
+    default: false
+  }
+})
+const isChecked = ref(props.checked)
+</script>
+<style scoped>
+#switch-label{
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1.5rem;
+}
+
+button {
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+}
+
+.track {
+  position: relative;
+  display: inline-block;
+  background-color: var(--border-main);
+  border-radius: var(--border-radius-large);
+  width: 4rem;
+  height: 2rem;
+}
+
+.thumb {
+  position: absolute;
+  cursor: pointer;
+  background-color: var(--switch-active);
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: var(--border-radius-large);
+  top: 50%;
+  transform: translateY(-50%);
+  left: 0.25rem;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+
+.is-active .thumb {
+  transform: translateX(1.75rem) translateY(-50%);
+  background-color: var(--border-main);
+}
+
+.is-active.track{
+  background-color: var(--switch-active);
+}
+</style>
