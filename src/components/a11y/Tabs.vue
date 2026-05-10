4991bc436260b7d55b67f504053683e0231ecbaf
Mirko Passeri <bionikle746@gmal.com>
2026-05-10 11:29:17 +0200

    Aggiunto il componente Tabs con gestione della navigazione tramite tasti e miglioramenti di accessibilità. Aggiornato il layout principale per includere un ID per il contenuto. Modificato il layout della documentazione per una struttura più chiara.

--- src/components/a11y/Tabs.vue
+++ src/components/a11y/Tabs.vue
@@ -1,18 +1,28 @@
 <template>
-  <div id="main-tablist" class="main-tablist" role="tablist">
-    <button v-for="(tab, index) in props.tabs" :key="index" :id="`tab-${index}`" role="tab"
+  <div class="tab-container">
+    <h1>Tabs</h1>
+  <div id="main-tablist" class="main-tablist" role="tablist" @keydown.right="activeTab === props.tabs.length - 1 ? activeTab = 0 : activeTab++" @keydown.left="activeTab === 0 ? activeTab = props.tabs.length - 1 : activeTab--">
+    <button :ref="el => tabRefs[index] = el" @click="activeTab = index" :aria-selected="index === activeTab ? true : false" v-for="(tab, index) in props.tabs" :key="index" :id="`tab-${index}`" role="tab"
       :aria-controls="`panel-${index}`" class="button-tab">
       {{ tab.title }}
     </button>
   </div>
-  <div v-for="(panel, index) in props.tabs" :key="index" :id="`panel-${index}`" role="tabpanel"
+  <div v-show="activeTab === index" v-for="(panel, index) in props.tabs" :key="index" :id="`panel-${index}`" role="tabpanel"
     :aria-labelledby="`tab-${index}`" class="container-tabpanel">
     {{ panel.content }}
+  </div>
   </div>
 </template>
 
 
 <script setup>
+import { ref, watch } from 'vue'
+const tabRefs = ref([])
+const activeTab = ref(0)
+
+watch(activeTab, (newValue) => {
+  tabRefs.value[newValue].focus()
+})
 const props = defineProps({
   tabs: {
     type: Array,
@@ -23,7 +33,35 @@ const props = defineProps({
     ]
   }
 })
+
 </script>
 
+<style scoped>
 
-<style scoped></style>
+h1{
+  margin-bottom: 1.5rem;
+}
+.tab-container{
+  max-width: 50%;
+  margin: 0;
+  max-height: 80%;
+}
+/* .container-tabpanel{
+background-color: var(--switch-active);
+color: var(--action-primary)
+} */
+
+.main-tablist{
+  display: flex;
+  gap: 0.2rem;
+}
+.button-tab {
+  margin-bottom: 1.5rem;
+  border-bottom: 3px solid transparent;
+}
+.button-tab[aria-selected="true"] {
+  background-color: var(--border-main);
+  color: var(--switch-active);
+  border-bottom: 3px solid var(--action-primary);
+}
+</style>
